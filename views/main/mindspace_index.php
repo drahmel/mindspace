a7fd9c6535b695253092b23849bf0240341ed07f
Dan Rahmel <drahmel7@gmail.com>
2013-11-02 09:53:27 -0700

    DJR:Refactored views

--- views/main/mindspace_index.php
+++ views/main/mindspace_index.php
@@ -4,10 +4,9 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Mindscape</title>
 
-    <!--Loading babylon engine -->
     <script src="/js/mindspace/babylon.js?v=1" ></script>
-    <!-- script src="http://www.babylonjs.com/tutorials/blogs/customShaders/babylon.js" ></script -->
     <script src="/js/mindspace/hand.js"></script>
+    <script src="/js/mindspace/core.js"></script>
 
 
     <!-- CSS -->
@@ -27,62 +26,15 @@
 <!-- BODY -->
 <!-- ------ -->
 <body>
-
-    <div id="rootDiv">
-
-        <!-- Main Canvas -->
-        <canvas id="renderCanvas"></canvas>
-    </div>
-
-    </body>
+	<div id="rootDiv">
+		<canvas id="renderCanvas"></canvas>
+	</div>
+</body>
 
 
 
-    <!-- BABYLON SCRIPT -->
-    <!-- -------------- -->
-    <script type="text/javascript">
-
-        // Get the Canvas element from our HTML below
-        var canvas = document.getElementById("renderCanvas");
-
-        // Load BABYLON 3D engine and set the root directory
-        var engine = new BABYLON.Engine(canvas, true);
-
-        //Create a new scene with a camera (mandatory), a light (better) and a sphere (to see the origin)
-        var scene = new BABYLON.Scene(engine);
-
-        // Creating a camera looking to the zero point (0,0,0)
-        var camera = new BABYLON.ArcRotateCamera("Camera", 1, 0.8, 10, new BABYLON.Vector3(0, 0, 0), scene);
-	// Creating a omnidirectional light
-	//var light0 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0, 0, 10), scene);
-	var plan = BABYLON.Mesh.CreatePlane("Plane", 50.0, scene);
-	plan.position = new BABYLON.Vector3(0, 50, 0);
-	var sun = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(60, 100, 10), scene);
-
-	camera.setPosition(new BABYLON.Vector3(-40, 40, 0));
-        
-        if(false) {
-		var skybox = BABYLON.Mesh.CreateBox("skyBox", 100.0, scene);
-		var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
-		skyboxMaterial.backFaceCulling = false;
-		skybox.material = skyboxMaterial;
-		skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
-		skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-		skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("/ui/images/mindspace/skybox/skybox", scene);
-		skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;	
-	} else {
-            // Skybox
-            var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
-            var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
-            skyboxMaterial.backFaceCulling = false;
-            skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("/ui/images/mindspace/skybox/skybox", scene);
-            skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-            skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
-            skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-            skybox.material = skyboxMaterial;
-		
-	}
-        // Creating a sphere of size 1, at 0,0,0
+<script type="text/javascript">
+    	init("renderCanvas");
 	<?php foreach($scene['objects'] as $object): ?>
 		<?php
 		$type = isset($object['type']) ? $object['type'] : 1;
@@ -117,29 +69,31 @@
 
         // Attach the camera to the scene
         scene.activeCamera.attachControl(canvas);
-
        
-var beforeRenderFunction = function () {
-                // Camera
-                if (camera.beta < 0.1)
-                    camera.beta = 0.1;
-                else if (camera.beta > (Math.PI / 2) * 0.9)
-                    camera.beta = (Math.PI / 2) * 0.9;
+	var beforeRenderFunction = function () {
+		// Camera
+		if (camera.beta < 0.1) {
+			camera.beta = 0.1;
+		} else if (camera.beta > (Math.PI / 2) * 0.9) {
+			camera.beta = (Math.PI / 2) * 0.9;
+		}
+		
+		if (camera.radius > 50) {
+			camera.radius = 50;
+		}
+		
+		if (camera.radius < 5) {
+			camera.radius = 5;
+		}
+	};
+	//scene.registerBeforeRender(beforeRenderFunction);
+	
+	// Once the scene is loaded, just register a render loop to render it
+	engine.runRenderLoop(function () {
+		scene.render();
+	});
 
-                if (camera.radius > 50)
-                    camera.radius = 50;
-
-                if (camera.radius < 5)
-                    camera.radius = 5;
-            };
-            //scene.registerBeforeRender(beforeRenderFunction);
-
-        // Once the scene is loaded, just register a render loop to render it
-        engine.runRenderLoop(function () {
-            scene.render();
-        });
-
-    </script>
+</script>
 
 
 </html>
